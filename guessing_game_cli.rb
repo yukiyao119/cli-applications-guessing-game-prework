7865301073272b3f36d89815f744ca099d8eaa04
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 21:55:37 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,5 +3,9 @@ def run_guessing_name
   num = rand(1..6)
   response = gets.strip
   if response.to_i == num 
+    puts "You guessed the correct number!"
+  elsif response == "exit"
+    puts "Goodbye!"
+  else
     puts ""
 end 
