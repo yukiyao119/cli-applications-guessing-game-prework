4b5c0bcda9c7704a90607fe70ae78c203c9e8d61
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 21:58:08 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def GuessingCLI
-  num = rand(1..6)
+  num = rand(1..6) + 1
   response = gets.strip
   if response.to_i == num 
     puts "You guessed the correct number!"
